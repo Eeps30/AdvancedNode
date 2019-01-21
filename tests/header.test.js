75e8010d4bd400ff6b020f113f5e049395319b3c
Eeps30 <eeps30@gmail.com>
2019-01-20 16:42:37 -0800

    created test with fake generated cookie and cookie signature to sign into localhost:3000

--- tests/header.test.js
+++ tests/header.test.js
@@ -11,7 +11,7 @@ beforeEach(async () => {
 })
 
 afterEach(async () => {
-    await browser.close();
+    // await browser.close();
 })
 
 test('Header contains text "Blogster"', async () => {
@@ -24,4 +24,28 @@ test('clicking login starts oauth flow', async () => {
 
     const url = await page.url();
     console.log(url);
+})
+
+test.only('when signed in, shows logout button', async () => {
+    const id = '5c24214cdad9632a6ac01b5c';
+
+    const Buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    } 
+
+    const sessionString = Buffer.from(
+        JSON.stringify(sessionObject)
+    ).toString('base64');
+    
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys');
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + sessionString);
+
+    await page.setCookie({ name: 'session', value: sessionString });
+    await page.setCookie({ name: 'session.sig', value: sig });
+    await page.goto('localhost:3000');
 })
