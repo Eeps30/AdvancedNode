50b01e9edbfbbceb98a2d2003079d2ad782c0c02
Eeps30 <eeps30@gmail.com>
2019-02-24 16:28:28 -0800

    changed settings in multiple files to get TravisCI working. Added a yaml file to configure TravisCI

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,6 @@ module.exports = {
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://eeps30:av862549@ds115963.mlab.com:15963/dev_blog',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -4,11 +4,44 @@ let page;
 
 beforeEach(async () => {
     page = await Page.build();
-    await page.goto('localhost:3000');
+    await page.goto('http://localhost:3000');
 })
 
 afterEach(async () => {
     await page.close();
+})
+
+describe('When logged out', async () => {
+    test('User cannot create blog posts', async () => {
+        const result = await page.evaluate(() => {
+            return fetch('/api/blogs', {
+                method: 'POST', 
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ title: 'My Title', content: 'My Content' })
+            }).then(res => res.json());
+        })
+
+        expect(result).toEqual({ error: 'You must log in!' });
+    })
+
+    test('User cannot get a list of posts', async () => {
+        const result = await page.evaluate(
+            () => {
+                return fetch('/api/blogs', {
+                    method: 'GET',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).then(res => res.json());
+            }
+        );
+
+        expect(result).toEqual({ error: 'You must log in!' });
+    })
 })
 
 describe('When logged in', async () => {
